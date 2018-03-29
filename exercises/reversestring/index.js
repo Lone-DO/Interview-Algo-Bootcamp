8db77a0154ce3302606d9a24a852ffe72fe62fd5
lone-DO <Lone-DO@users.noreply.github.com>
2018-03-29 17:58:33 -0400

    Solving First Algorithm "Reversing Strings"

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -1,3 +1,4 @@
+/**
 // --- Directions
 // Given a string, return a new string with the reversed
 // order of characters
@@ -5,7 +6,42 @@
 //   reverse('apple') === 'leppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
+**/
 
-function reverse(str) {}
+// SOLUTION #1 W/ HELPER
+// function reverse(str) {
+//    // return str
+//    //    .split("")
+//    //    .reverse()
+//    //    .join("");
+
+//    //OR
+//    // let splitStr = str.split(""),
+//    //    reverseArr = splitStr.reverse(),
+//    //    result = reverseArr.join("");
+//    // return result;
+// }
+
+// --------------
+
+// SOLUTION #2 W/ FOR LOOP
+// function reverse(str) {
+//
+//    let reversed = "";
+//    for (let char of str) {
+//       reversed = char + reversed;
+//    }
+
+//    return reversed;
+
+// }
+
+// --------------
+
+// SOLUTION #3 W/ REDUCE
+function reverse(str) {
+   // Manual Solution
+   return str.split("").reduce((rev, char) => char + rev, "");
+}
 
 module.exports = reverse;
