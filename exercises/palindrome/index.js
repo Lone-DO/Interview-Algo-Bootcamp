6b662da761532d4f51e3780348fd09379f6f9ce5
lone-DO <Lone-DO@users.noreply.github.com>
2018-03-29 18:51:39 -0400

    Completed Palindrome Algo

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,39 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// --------
+
+//SOLUTION #1 W/ If Statment
+//function palindrome(str) {
+// let revStr = str
+//       .split("")
+//       .reverse()
+//       .join("");
+
+//    if (str == revStr) {
+//       return true;
+//    } else {
+//       return false;
+//    }
+//}
+
+// --------
+
+//SOLUTION #2 W/ return boolean
+//function palindrome(str) {
+// let revStr = str
+//       .split("")
+//       .reverse()
+//       .join("");
+
+//    return str === revsersed;
+//}
+
+//SOLUTION #3 W/ If Statment
+function palindrome(str) {
+   return str.split("").every((char, i) => {
+      return char === str[str.length - i - 1];
+   });
+}
 
 module.exports = palindrome;
